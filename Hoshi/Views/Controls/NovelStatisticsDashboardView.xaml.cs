9c1c58c1830d5f8f4513752c390c8b5bb14a3156
shishamo <506997957@qq.com>
2026-07-11 12:40:11 +0800

    feat(statistics): add full-page dashboard surface

---
+++ Hoshi/Views/Controls/NovelStatisticsDashboardView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Hoshi.Views.Controls;
+
+public sealed partial class NovelStatisticsDashboardView : UserControl
+{
+    public NovelStatisticsDashboardView()
+    {
+        InitializeComponent();
+    }
+}
